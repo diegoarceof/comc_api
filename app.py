1772673204d9fc1f58ed7e0579cb7b6f6f0e8453
Diego Arceo <diego@hal9.ai>
2025-02-25 15:41:34 -0600

    fixed app

--- app.py
+++ app.py
@@ -10,10 +10,3 @@ def hello():
 @app.get("/greet/{name}")
 def greet(name: str):
     return {"message": f"Hello, {name}!"}   
-
-@app.post("/add")
-def add(data: dict):
-    return {"result": data["num1"] + data["num2"]}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
