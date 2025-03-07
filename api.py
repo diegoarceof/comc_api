8c2710747e8e28a560d29d1acab8b4236fb1bd45
Diego Arceo <diego@hal9.ai>
2025-03-07 09:19:32 -0600

    Added comments

--- api.py
+++ api.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-from enum import Enum
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 from search import query
 
+# Define API parameters
 class QueryParams(BaseModel):
     embeddings: list[list[float]]
     n_neighbors: int = 5
@@ -13,17 +13,19 @@ class QueryParams(BaseModel):
 
 app = FastAPI()
 
+# Root endpoint to test connection
 @app.get("/")
 def root():
     return {"message": "COMC nearest neighbor search"}
 
+# Endpoint to query the database
 @app.post("/nearest_neighbors")
 async def nearest_neighbors(params: QueryParams):
     embeddings = np.array(params.embeddings)
     n_neighbors = params.n_neighbors
     
     n_cpus = params.n_cpus
-    
+
     distances, images = query(embeddings, n_neighbors, n_cpus)
     return {
         'distances': distances.tolist(),
--- search.py
+++ search.py
@@ -1,7 +1,9 @@
 import faiss
 import numpy as np
 
+# Function to create index
 def create_index():
+    # Load the database and normalize it. No need to reshape, it is already 2D
     embeddings_database = np.load('../comc_images/swim_embeddings.npy')
     embeddings_database /= np.linalg.norm(embeddings_database, keepdims=True)
     dimensions = embeddings_database.shape[1]
@@ -12,20 +14,15 @@ def create_index():
 
     return index
 
+# Load image names and index
 image_names = np.load('../comc_images/image_names.npy')
 index = create_index()
 
-# Query the embeddings
+# Function to query the embeddings
 def query(embeddings: np.array, n_neighbors: int, n_cpus: int = 3):
     faiss.omp_set_num_threads(n_cpus)
-    print(f'[Search] Initializing query search with {n_cpus} CPUs')
 
     distances, indices = index.search(embeddings, n_neighbors)
     names = image_names[indices]
-    print(f'[Search] Found {n_neighbors} neighbors')
-
-    print(f'[Search] First 3 indices {indices[:, :3]}')
-    print(f'[Search] {distances = }')
-    print(f'[Search] First three image names: {names[:,:3]}', end = '\n\n')
 
     return distances, names
