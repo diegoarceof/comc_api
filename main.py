cfeb9eb2370136acb085683cc270a96e3db6d4db
Diego Arceo <diego@hal9.ai>
2025-03-06 18:28:04 -0600

    Made main an API to host on hal9001

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import argparse
-import asyncio
 import aiohttp
+import asyncio
 import time
 
 import numpy as np
+
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+from image_utils import get_embeddings
 
 async def post_url(session, url, payload):
     async with session.post(url, json=payload) as response:
@@ -15,8 +19,7 @@ async def get_url(session, url):
         if response.headers.get('Content-Type', '').startswith('application/json'):
             return await response.json()
         else:
-            # Handle other types of responses (e.g., image, text, etc.)
-            return await response.read()  # Read the raw content
+            return await response.read() 
 
 async def call_urls(*urls, payload = None):    
     async with aiohttp.ClientSession() as session:
@@ -31,7 +34,7 @@ async def call_urls(*urls, payload = None):
         results = await asyncio.gather(*tasks)
         return results
     
-async def main(embeddings: list, n_neighbors: int, n_cpus: int):
+async def main(embeddings: np.array, n_neighbors: int, n_cpus: int):
     t0 = time.perf_counter()
     # List of your API endpoints
     base_url = 'http://compute.hal9.com:{port}/nearest_neighbors'
@@ -59,31 +62,41 @@ async def main(embeddings: list, n_neighbors: int, n_cpus: int):
     sorted_indices = np.argsort(distances, axis=1)
     sorted_images = np.take_along_axis(images, sorted_indices, axis=1)
 
-    n_images = len(embeddings)
+    n_images = embeddings.shape[0]
     images_base_url = 'https://temphal9.s3.us-west-2.amazonaws.com/comc/data/0.0.1/extracted/'
 
     image_urls = np.char.add(images_base_url,  sorted_images[:,:n_neighbors].reshape(n_images*n_neighbors))    
 
     response = await call_urls(*image_urls)
-    # print(response)
-
+    arr_response = np.array(response).reshape((n_images, n_neighbors))
+    
     t1 = time.perf_counter()
     print(f'Time formatting and downloading images: {t1-end_time:.3f} seconds')
     print(f"Total time taken: {t1 - t0:.3f} seconds")
 
-# Run the async main function
+    return arr_response
+
+class MainParams(BaseModel):
+    encoded_images: list[str]
+    n_neighbors: int
+    n_cpus: int
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "COMC nearest neighbor search"}
+
+@app.post("/nearest_neighbors")
+def call_apis(params: MainParams):
+    encoded_images = params.encoded_images
+    n_neighbors = params.n_neighbors
+    n_cpus = params.n_cpus
+    
+    embeddings = get_embeddings(encoded_images)
+
+    return asyncio.run(main(embeddings, n_neighbors, n_cpus))
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--n_images', type=int, default=3, help='Number of images')
-    parser.add_argument('-n', '--n_neighbors', type=int, default=5, help='Number of neighbors')
-    parser.add_argument('-c', '--n_cpus', type=int, default=4, help='Number of CPUs')
-    args = parser.parse_args()
-
-    np.random.seed(0)
-
-    n_images = args.n_images
-    n_neighbors = args.n_neighbors
-    n_cpus = args.n_cpus
-
-    embeddings = np.random.rand(n_images, 75264).tolist()
-    asyncio.run(main(embeddings, n_neighbors, n_cpus))
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
