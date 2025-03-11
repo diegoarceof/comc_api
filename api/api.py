72b3eeafaf240725553c3c44167128252deb40e5
Diego Arceo <diego@hal9.ai>
2025-03-11 17:49:47 -0600

    testing params

--- api/api.py
+++ api/api.py
@@ -1,17 +1,9 @@
 import numpy as np
 
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 from search import query
 from train import save
-
-# Define API parameters
-class QueryParams(BaseModel):
-    embeddings: list[list[float]]
-    n_neighbors: int = 5
-    n_cpus: int = 3
-    database_name: str = 'COMC'
 
 app = FastAPI()
 
@@ -22,11 +14,13 @@ def root():
 
 # Endpoint to query the database
 @app.post("/nearest_neighbors")
-async def nearest_neighbors(params: QueryParams):
-    embeddings = np.array(params.embeddings)
-    n_neighbors = params.n_neighbors
-    
-    n_cpus = params.n_cpus
+async def nearest_neighbors(
+        embeddings: list[list[float]],
+        n_neighbors: int = 5,
+        n_cpus: int = 3,
+        database_name: str = 'COMC'):
+
+    embeddings = np.array(embeddings)
 
     distances, images = query(embeddings, n_neighbors, n_cpus)
     return {
