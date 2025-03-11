526b0f30c3c40d3f253ec77ccf2cd80f4d68da8c
Diego Arceo <diego@hal9.ai>
2025-03-11 17:25:19 -0600

    bug logs

--- api/api.py
+++ api/api.py
@@ -41,7 +41,7 @@ async def get_length():
 @app.post("/save_embeddings")
 async def save_embeddings(embeddings: list[list[float]], database_name: str):
     try:
-        save(embeddings)
+        save(embeddings, database_name)
         return {'content': 'Database updated succesfully'}, 200
     except Exception as e:
         return {'error': f'Error saving embeddings: {str(e)}'}, 400
--- api/train.py
+++ api/train.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def save(embeddings: list[list[float]]):
+def save(embeddings: list[list[float]], database_name: str):
     embeddings = np.array(embeddings)
 
     current = np.load("../../comc_embeddings", mmap_mode='r+')
     updated = np.concatenate((current, embeddings), axis = 0)
 
-    print(f'Saving new embeddings. New total: {updated.shape[0] + current.shape[0]}')
+    print(f'Saving new embeddings on {database_name}. New total: {updated.shape[0] + current.shape[0]}')
     # np.save("../../comc_embeddings", axis = 0)
